21f9c4b6337935539d8a104b729a0436899b32aa
KCrowe03 <165347733+KCrowe03@users.noreply.github.com>
2024-09-21 19:17:05 -0400

    completed assignment

--- reviews.py
+++ reviews.py
@@ -1,2 +1,22 @@
-# add your code here
+# Kristen Crowe 
+# 9/18/24
 
+import pandas as pd 
+
+df = pd.read_csv('data/winemag-data-130k-v2.csv.zip')
+
+# Group-by country to get the count of reviews average points
+summary_df = df.groupby('country').agg(
+    count=('country', 'size'),  
+    points=('points', 'mean')   
+).reset_index()
+
+# Round the average points to 1 decimal point
+summary_df['points'] = summary_df['points'].round(1)
+
+# Use to check column names in data set
+#print(df.columns)
+
+print(summary_df.head())
+
+summary_df.to_csv('data/reviews-per-country.csv', index=False)
